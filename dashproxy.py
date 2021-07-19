b8961b1ed186e81eb896494f335ad655b007dcdd
Anton Avramov <lukav@lukav.com>
2021-07-19 13:36:21 -0400

    Fix if there is no ? in the url rfind would return -1 and trim the last char of the filename

--- dashproxy.py
+++ dashproxy.py
@@ -259,7 +259,7 @@ class DashDownloader(HasLogger):
         return self.mpd_base_url + dest # TODO remove hardcoded arrd
 
     def write(self, dest, content):
-        dest = dest[0:dest.rfind('?')]
+        dest = dest.split('?')[0]
         dest = os.path.join(self.proxy.output_dir, dest)
         os.makedirs(os.path.dirname(dest), exist_ok=True)
         f = open(dest, 'wb')
